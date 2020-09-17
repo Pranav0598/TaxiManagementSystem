448dcb002a2291f387cfdafa484c20dade37f5a0
Pranav <pranavsai.gubbala@gmail.com>
2020-09-18 04:15:40 +0930

    Project setup

---
+++ TaxiManagementSystem/Models/TaxiManagementSystemContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TaxiManagementSystem.Models
+{
+    public partial class TaxiManagementSystemContext : DbContext
+    {
+        public TaxiManagementSystemContext()
+        {
+        }
+
+        public TaxiManagementSystemContext(DbContextOptions<TaxiManagementSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__Users__1788CC4C3D84AFB1");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ TaxiManagementSystem/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaxiManagementSystem.Models
+{
+    public partial class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int? Age { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
